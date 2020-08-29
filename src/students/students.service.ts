feb1414e75e216439a91c33a57f084c25ff518c4
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-29 09:02:10 -0500

    [ADD] working service

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,69 +1,69 @@
 import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
-import { InjectModel } from '@nestjs/mongoose';
+// import { InjectModel } from '@nestjs/mongoose';
 
-import { Student } from './interface/student.interface';
-import { CreateStudentDTO } from './dto/student.dto';
-import { Course } from 'src/courses/interfaces/course.interface';
+// import { Student } from './interface/student.interface';
+import { UserDto } from './dto/student.dto';
+// import { Course } from 'src/courses/interfaces/course.interface';
 
 @Injectable()
 export class StudentsService {
-  constructor(
-    @InjectModel('Student') private studentModel: Model<Student>,
-    @InjectModel('Course') private courseModel: Model<Course>,
-  ) {}
+  // constructor(
+  //   @InjectModel('Student') private studentModel: Model<Student>,
+  //   // @InjectModel('Course') private courseModel: Model<Course>,
+  // ) {}
 
-  async getStudents(): Promise<Student[]> {
-    const students = await this.studentModel.find();
-    return students;
-  }
+  // async getStudents(): Promise<Student[]> {
+  //   const students = await this.studentModel.find();
+  //   return students;
+  // }
 
-  async createStudent(createStudentDTO: CreateStudentDTO): Promise<Student> {
-    const createdStudent = new this.studentModel(createStudentDTO);
-    return await createdStudent.save();
-  }
+  // async createStudent(createStudentDTO: CreateStudentDTO): Promise<Student> {
+  //   const createdStudent = new this.studentModel(createStudentDTO);
+  //   return await createdStudent.save();
+  // }
 
-  async getStudent(studentID: string): Promise<Student> {
-    const student = await this.studentModel.findById(studentID);
-    return student;
-  }
+  // async getStudent(studentID: string): Promise<Student> {
+  //   const student = await this.studentModel.findById(studentID);
+  //   return student;
+  // }
 
-  async updateStudent(
-    studentID: string,
-    createStudentDTO: CreateStudentDTO,
-  ): Promise<Student> {
-    const updatedStudent = await this.studentModel.findOneAndUpdate(
-      { _id: studentID },
-      createStudentDTO,
-      { new: true },
-    );
-    return updatedStudent;
-  }
+  // async updateStudent(
+  //   studentID: string,
+  //   createStudentDTO: CreateStudentDTO,
+  // ): Promise<Student> {
+  //   const updatedStudent = await this.studentModel.findOneAndUpdate(
+  //     { _id: studentID },
+  //     createStudentDTO,
+  //     { new: true },
+  //   );
+  //   return updatedStudent;
+  // }
 
-  async deleteStudent(studentID: string): Promise<Student> {
-    const deletedStudent = await this.studentModel.findByIdAndDelete({
-      _id: studentID,
-    });
-    return deletedStudent;
-  }
+  // async deleteStudent(studentID: string): Promise<Student> {
+  //   const deletedStudent = await this.studentModel.findByIdAndDelete({
+  //     _id: studentID,
+  //   });
+  //   return deletedStudent;
+  // }
 
-  async assignStudentToCourse(studentID: string, courseID: string) {
-    const student = await this.studentModel.findOneAndUpdate(
-      { _id: studentID },
-      { $push: { courses: courseID } },
-    );
-    if (!student) {
-      return false;
-    }
+  // async assignStudentToCourse(studentID: string, courseID: string) {
+  //   const student = await this.studentModel.findOneAndUpdate(
+  //     { _id: studentID },
+  //     { $push: { courses: courseID } },
+  //   );
+  //   if (!student) {
+  //     return false;
+  //   }
 
-    const course = await this.courseModel.findOneAndUpdate(
-      { _id: courseID },
-      { $inc: { students: 1 } },
-    );
-    if (!course) {
-      return false;
-    }
+  //   const course = await this.courseModel.findOneAndUpdate(
+  //     { _id: courseID },
+  //     { $inc: { students: 1 } },
+  //   );
+  //   if (!course) {
+  //     return false;
+  //   }
 
-    return true;
-  }
+  //   return true;
+  // }
 }
