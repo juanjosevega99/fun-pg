d741d3b1db2d4193eeb42c5c4324768c42c18c5a
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-28 22:39:18 -0500

    [FEATURE] new database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,24 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import configuration from './config/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { StudentsModule } from './students/students.module';
 import { CoursesModule } from './courses/courses.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      load: [configuration],
-    }),
-    MongooseModule.forRootAsync({
+    TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGODB_URI'),
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useCreateIndex: true
+        useFactory: (configService: ConfigService) => ({
+          type: 'postgres',
+          host: configService.get('HOST'),
+          port: 5432,
+          username: configService.get('USERNAME'),
+          password: configService.get('PASSWORD'),
+          database: configService.get('DATABASE'),
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          migrations: [__dirname + '/migrations/*{.ts,.js}']
       }),
       inject: [ConfigService]
     }),
