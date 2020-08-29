3decda60304b94c45aff70f34577c8b4e3873298
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-29 09:04:03 -0500

    [DELETE]  files to work project

--- src/courses/dto/course.dto.ts
+++
@@ -1,6 +0,0 @@
-export class CreateCourseDTO {
-  name: string;
-  schedule: string;
-  dateStart: Date;
-  dateEnd: Date;
-}
--- src/courses/interfaces/course.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface Course extends Document {
-  name: string;
-  schedule: string;
-  dateStart: Date;
-  dateEnd: Date;
-  students: number
-}
