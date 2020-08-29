2a1b8a033aa6df3e6e0c5a384f0c1005dd0ab829
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-29 09:00:11 -0500

    [ADD] new keys enviaronment

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
-export enum Configuration {
-  PORT = 'PORT',
-  HOST = 'HOST',
-  USERNAME = 'USERNAME',
-  PASSWORD = 'PASSWORD',
-  DATABASE = 'DATABASE',
-}
+export default () => ({
+  port: process.env.PORT || '',
+  host: process.env.HOST || '',
+  username: process.env.USERNAME || '',
+  password: process.env.PASSWORD || '',
+  db: process.env.DATABASE || '',
+});
