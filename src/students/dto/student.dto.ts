947b30b5077e73d7d1aab2021c5c5caf8f7484ba
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-29 09:01:42 -0500

    [ADD] new dto student

--- src/students/dto/student.dto.ts
+++ src/students/dto/student.dto.ts
@@ -1,6 +1,15 @@
-export class CreateStudentDTO {
+import { IsNotEmpty } from "class-validator";
+
+export class UserDto {
+  @IsNotEmpty()
   name: string
+
+  @IsNotEmpty()
   lastName: string
+
+  @IsNotEmpty()
   age: number
+
+  @IsNotEmpty()
   email: string
 }
