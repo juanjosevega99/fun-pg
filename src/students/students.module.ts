983bec98c363926527975d1f14bda3cd66959a4c
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-29 09:02:43 -0500

    [ADD] working module

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
-import { StudentSchema } from './schemas/student.schema';
-import { CourseSchema } from 'src/courses/schemas/course.schema';
+// import { StudentSchema } from './schemas/student.schema';
+// import { CourseSchema } from 'src/courses/schemas/course.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      { name: 'Student', schema: StudentSchema },
-      { name: 'Course', schema: CourseSchema },
-    ]),
+    // MongooseModule.forFeature([
+    //   // { name: 'Student', schema: StudentSchema },
+    //   { name: 'Course', schema: CourseSchema },
+    // ]),
   ],
   controllers: [StudentsController],
   providers: [StudentsService],
